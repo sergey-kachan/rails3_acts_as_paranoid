a109696d4a45e69127261b7c368683880f51123c
Sergey Kachan <sk@anahoret.com>
2012-07-20 11:20:32 +0300

    Backport :with_deleted option to belongs_to.

--- lib/rails3_acts_as_paranoid.rb
+++ lib/rails3_acts_as_paranoid.rb
@@ -93,9 +93,33 @@ module ActsAsParanoid
     
     include InstanceMethods
     extend ClassMethods
+
+    class << self
+      alias_method_chain :belongs_to, :deleted
+    end
   end
 
   module ClassMethods
+    def belongs_to_with_deleted(target, options = {})
+      with_deleted = options.delete(:with_deleted)
+      result = belongs_to_without_deleted(target, options)
+
+      if with_deleted
+        result.options[:with_deleted] = with_deleted
+        class_eval <<-RUBY, __FILE__, __LINE__
+          def #{target}_with_unscoped(*args)
+            association = association(:#{target})
+            return nil if association.options[:polymorphic] && association.klass.nil?
+            return #{target}_without_unscoped(*args) unless association.klass.paranoid?
+            association.klass.with_deleted.scoping { #{target}_without_unscoped(*args) }
+          end
+          alias_method_chain :#{target}, :unscoped
+        RUBY
+      end
+
+      result
+    end
+
     def self.extended(base)
       base.define_callbacks :recover
     end
